ed248a4786010251df3176e4e3e222eb3ed3f120
catalin.croitoru <catalin.croitoru@gmail.com>
2026-03-04 18:49:52 +0200

    add maven configuration, Encoder scheleton and EncoderTest with tests.

---
+++ src/main/java/ro/catta/interview/ssc/Encoder.java
@@ -0,0 +1,9 @@
+package ro.catta.interview.ssc;
+
+public class Encoder {
+
+	public String encode(String in) {
+		throw new RuntimeException("not implemented!");
+	}
+
+}
---
+++ src/test/java/ro/catta/interview/ssc/EncoderTest.java
@@ -0,0 +1,35 @@
+package ro.catta.interview.ssc;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class EncoderTest {
+
+	static Stream<Arguments> testEncodeParams() {
+		return Stream.of(//
+				Arguments.of("aaaabbbccc", "a4b3c3"), //
+				Arguments.of("abbbcdddd", "a1b3c1d4"), //
+				Arguments.of("aaabbwwaa", "a3b2w2a2"), //
+				Arguments.of("a", "a1"), //
+				Arguments.of("", ""));
+	}
+
+	@ParameterizedTest
+	@MethodSource("testEncodeParams")
+	public void testEncode(String input, String expected) {
+		// Arrange
+		Encoder underTest = new Encoder();
+
+		// Act
+		String actual = underTest.encode(input);
+
+		// Assert
+		Assertions.assertEquals(expected, actual, () -> String.format("for input %s", input));
+
+	}
+
+}
