504c100a8e6136e5a126d87738ea741fb638ebf7
catalin.croitoru <catalin.croitoru@gmail.com>
2026-03-05 00:45:36 +0200

    format with spaces

--- src/test/java/ro/catta/interview/ssc/EncoderTest.java
+++ src/test/java/ro/catta/interview/ssc/EncoderTest.java
@@ -9,42 +9,42 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class EncoderTest {
 
-	static Stream<Arguments> testEncodeParams() {
-		return Stream.of(//
-				Arguments.of("aaaabbbccc", "a4b3c3"), //
-				Arguments.of("abbbcdddd", "a1b3c1d4"), //
-				Arguments.of("aaabbwwaa", "a3b2w2a2"), //
-				Arguments.of("a", "a1"), //
-				Arguments.of("aa", "a2") //
-		);
-	}
+    static Stream<Arguments> testEncodeParams() {
+        return Stream.of(//
+                Arguments.of("aaaabbbccc", "a4b3c3"), //
+                Arguments.of("abbbcdddd", "a1b3c1d4"), //
+                Arguments.of("aaabbwwaa", "a3b2w2a2"), //
+                Arguments.of("a", "a1"), //
+                Arguments.of("aa", "a2") //
+        );
+    }
 
-	@ParameterizedTest
-	@MethodSource("testEncodeParams")
-	public void testEncode(String input, String expected) {
-		// Arrange
-		Encoder underTest = new Encoder();
+    @ParameterizedTest
+    @MethodSource("testEncodeParams")
+    public void testEncode(String input, String expected) {
+        // Arrange
+        Encoder underTest = new Encoder();
 
-		// Act
-		String actual = underTest.encode(input);
+        // Act
+        String actual = underTest.encode(input);
 
-		// Assert
-		Assertions.assertEquals(expected, actual, () -> String.format("for input %s", input));
+        // Assert
+        Assertions.assertEquals(expected, actual, () -> String.format("for input %s", input));
 
-	}
+    }
 
-	@ParameterizedTest
-	@MethodSource("testEncodeParams")
-	public void testEncodeWithStream(String input, String expected) {
-		// Arrange
-		Encoder underTest = new Encoder();
+    @ParameterizedTest
+    @MethodSource("testEncodeParams")
+    public void testEncodeWithStream(String input, String expected) {
+        // Arrange
+        Encoder underTest = new Encoder();
 
-		// Act
-		String actual = underTest.encodeWithStream(input);
+        // Act
+        String actual = underTest.encodeWithStream(input);
 
-		// Assert
-		Assertions.assertEquals(expected, actual, () -> String.format("for input %s", input));
+        // Assert
+        Assertions.assertEquals(expected, actual, () -> String.format("for input %s", input));
 
-	}
+    }
 
 }
